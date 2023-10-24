927680a1cbea6b737477f0be5c7dd287fedc61f1
Fiorela <paolacapira.10@gmail.com>
2023-10-24 18:27:45 -0500

    firs

--- src/componets/AppForm.js
+++ src/componets/AppForm.js
@@ -1,13 +1,34 @@
 import React from 'react'
 
 const AppForm = () => {
+  const handleSubmit = (e) =>{ 
+    e.preventDefault();
+    try {
+      if(db){
+        console.log("Se guardo exitosamente");
+      }
+
+  } catch (error) {
+    console.error();
+  }
+
+  }
   return (
     <div style={{background:"orange", padding:"10px", textAlign:"center"}}>
+    <form onSubmit={handleSubmit}>
       <h1>AppForm.js</h1>
-      <p>Nombre</p>
-      <p>Edad</p>
-      <p>Genero</p>
-      <p>Guardar</p>
+      <input type='texto' placeholder='Nombre.....'/>
+      <input type='texto' placeholder='Nombre.....'/>
+      <select>
+      <option value="">Seleccione</option>
+      <option value="M">Masculino</option>
+      <option value="F">Femenino</option>
+      <option value="N">No Binario</option>
+      </select>
+      <button>
+        Guardar/Actualizar
+      </button>
+    </form>
     </div>
   )
 }
--- src/componets/firebase.js
+++ src/componets/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAVqOuH7j-XB96CPF7Lb0lnpVxyb7yx2oA",
-  authDomain: "appreact-1f782.firebaseapp.com",
-  projectId: "appreact-1f782",
-  storageBucket: "appreact-1f782.appspot.com",
-  messagingSenderId: "844955147948",
-  appId: "1:844955147948:web:6998a837c194cf7bff8909"
+  apiKey: "AIzaSyCYsuKfRhluC31NmP8kuL6Zjy9LZruJNao",
+  authDomain: "hola-47adc.firebaseapp.com",
+  projectId: "hola-47adc",
+  storageBucket: "hola-47adc.appspot.com",
+  messagingSenderId: "915989820485",
+  appId: "1:915989820485:web:cfbef36f9326d300f99f2c",
+  measurementId: "G-TSJNTYF84H"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
