cab36bb3ce023ae0a9a9e19db9693f2a01c61fa2
Fiorela <paolacapira.10@gmail.com>
2023-10-24 19:30:50 -0500

    firsr

--- src/componets/AppForm.js
+++ src/componets/AppForm.js
@@ -1,36 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { db } from './firebase';
+import { addDoc, collection } from 'firebase/firestore';
+
 
 const AppForm = () => {
-  const handleSubmit = (e) =>{ 
-    e.preventDefault();
-    try {
-      if(db){
-        console.log("Se guardo exitosamente");
-      }
 
-  } catch (error) {
-    console.error();
-  }
+    ////////////////GUARDAR O ACTUALIZAR//////////////////
+    const composRegistro = {Nombre:"", Edad:"",Genero:""}
+    const[objeto, setObjeto] = useState(composRegistro);
+    const handleSubmit= (e) => {
+        e.preventDefault();
 
-  }
-  return (
-    <div style={{background:"orange", padding:"10px", textAlign:"center"}}>
-    <form onSubmit={handleSubmit}>
-      <h1>AppForm.js</h1>
-      <input type='texto' placeholder='Nombre.....'/>
-      <input type='texto' placeholder='Nombre.....'/>
-      <select>
-      <option value="">Seleccione</option>
-      <option value="M">Masculino</option>
-      <option value="F">Femenino</option>
-      <option value="N">No Binario</option>
-      </select>
-      <button>
-        Guardar/Actualizar
-      </button>
-    </form>
-    </div>
-  )
+        try {
+            if (db){
+                addDoc(collection(db,'persona'), objeto);
+                console.log("Se guardo con éxito");
+            }
+        } catch (error) {
+            console.error();
+        }
+
+    }     
+
+    return (
+        <div style={{background:"orange",padding:"10px",textAlign:"center"}}>
+            <form onSubmit={handleSubmit}>
+                <h1>AppForm.js</h1>
+                <input type='text' placeholder='Nombre...'/>
+                <input type='text' placeholder='Apellidos...'/>
+                <select>
+                    <option value="">Seleccione...</option>
+                    <option value="M">Masculino</option>
+                    <option value="F">Femenino</option>
+                    <option value="N">No binario</option>
+                 
+                </select>
+                <button>
+                    Guardar / Actualizar
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default AppForm
